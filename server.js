3514dbfbddbca4fd7e8b5c95163e1e34e8ccd7db
kmurata798 <kmurata798@gmail.com>
2020-07-10 21:01:12 -0700

    [Add] Test HTML template route showing

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// MIDDLEWARE
+var exphbs = require('express-handlebars');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('posts-index', { msg: 'Handlebars are Cool!' });
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
