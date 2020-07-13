00531c7698a8e48ab4fce6dfbd297f3b9bc2168c
kmurata798 <kmurata798@gmail.com>
2020-07-12 22:14:56 -0700

    [Add] Post test + authentication tests all passing

--- test/posts.js
+++ test/posts.js
@@ -3,23 +3,42 @@ const app = require("./../server");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const agent = chai.request.agent(app);
+
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require('../models/user');
 const server = require('../server');
 
 chai.should();
 chai.use(chaiHttp);
 
 describe('Posts', function() {
-    const agent = chai.request.agent(server);
+    // const agent = chai.request.agent(server);
     // Post that we'll use for testing purposes
     const newPost = {
         title: 'post title',
         url: 'https://www.google.com',
         summary: 'post summary'
     };
+    const user = {
+        username: 'poststest',
+        password: 'testposts'
+    };
+    before(function (done) {
+        agent
+            .post('/sign-up')
+            .set("content-type", "application/x-www-form-urlencoded")
+            .send(user)
+            .then(function (res) {
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
     it('Should create with valid attributes at POST /posts/new', function(done) {
         // Checks how many posts there are now
         Post.estimatedDocumentCount()
@@ -52,7 +71,23 @@ describe('Posts', function() {
                 done(err);
             });
     });
-    after(function () {
-        Post.findOneAndDelete(newPost);
+    after(function (done) {
+        Post.findOneAndDelete(newPost)
+        .then(function (res) {
+            agent.close()
+      
+            User.findOneAndDelete({
+                username: user.username
+            })
+              .then(function (res) {
+                  done()
+              })
+              .catch(function (err) {
+                  done(err);
+              });
+        })
+        .catch(function (err) {
+            done(err);
+        });
     });
 });
